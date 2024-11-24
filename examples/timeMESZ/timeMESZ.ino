6a172b9bd13cfb651500f75ebc0ee51ab6f3afdc
Hasenradball <mail@frankhaefele.de>
2024-11-24 23:01:41 +0100

    correction of typos

--- examples/timeMESZ/timeMESZ.ino
+++ examples/timeMESZ/timeMESZ.ino
@@ -35,10 +35,10 @@ void setup() {
    while (!MESZ::get_timeHasBeenSynchronized()) {
       yield();
    }
-   Serial.println("Time successfully syncronized!");
+   Serial.println("Time successfully synchronized!");
    MESZ::set_timeHasBeenSynchronized_false();
    auto syncTime_ms = sntp_get_sync_interval();
-   Serial.printf("\tthe sntp sync intervall is set to : %ld ms / %ld s / %ld min / %ld h\n", \
+   Serial.printf("\tthe sntp sync interval is set to : %ld ms / %ld s / %ld min / %ld h\n", \
    syncTime_ms, syncTime_ms/1000U, syncTime_ms/60000U, syncTime_ms/3600000U);
 
    Serial.printf("\nZeit: %s", uhr.get_asctime());
@@ -47,7 +47,7 @@ void setup() {
 
 void loop() {
    if (MESZ::get_timeHasBeenSynchronized()) {
-      Serial.println("Time successfully syncronized!\n");
+      Serial.println("Time successfully synchronized!\n");
       MESZ::set_timeHasBeenSynchronized_false();
       Serial.printf("\n Zeit: %s", uhr.get_asctime());
    }
